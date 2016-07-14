9a6fdd97cd961648bd601fb1339074c8d5d5ed69
lilinchuan <lilinchuan@hixiaole.com>
2016-07-14 18:27:07 +0800

    fix

--- src/main/java/com/llc/utils/Utils.java
+++ src/main/java/com/llc/utils/Utils.java
@@ -26,10 +26,10 @@ public class Utils {
     public static final String logstore = "logstore"; // 上面步骤创建的日志库名称
 
     public static File convert(MultipartFile file) throws IOException {
-
         String name = file.getOriginalFilename();
         File readFile = new File(name);
-        InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(readFile));
+        logger.info("open file: "+name);
+        InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(readFile),"UTF-8");
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         String line = bufferedReader.readLine();
         logger.info("Original sentence: "+line);
@@ -39,29 +39,8 @@ public class Utils {
                 new FileOutputStream(new File(name)));
         FileCopyUtils.copy(file.getInputStream(), stream);
         stream.close();
-
         File inputFile = new File(name);
         return inputFile;
-//        BufferedReader reader = null;
-//
-//        String output = "";
-//        try {
-//            reader = new BufferedReader(new FileReader(inputFile));
-//            String line = "";
-//            while ((line = reader.readLine()) != null) {
-//
-//            }
-
-//        File convFile = new File(file.getOriginalFilename());
-//        boolean b = convFile.createNewFile();
-//        if (!b) {
-//            logger.info(file.getOriginalFilename() + " can't trans form to file");
-//        }
-//        FileOutputStream fos = new FileOutputStream(convFile);
-//        fos.write(file.getBytes());
-//        logger.info("Read contents: " + file.getBytes());
-//        fos.close();
-//        return convFile;
     }
 
     public static void writeToTxt(File file) throws IOException{
